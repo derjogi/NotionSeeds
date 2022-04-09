407ae2c7fc41764de42c9bfeb6fd6d5dfe31845c
derjogi <derjogi@web.de>
2022-04-09 15:36:04 +1200

    🧹

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -5,55 +5,6 @@ import { FaHamburger } from '@react-icons/all-files/fa/FaHamburger'
 import { FaAngleRight } from '@react-icons/all-files/fa/FaAngleRight'
 import Nav from '../lib/navigation.json'
 
-// import useSWR from 'swr'
-// import { getSiteMap } from '../lib/cache'
-// import axios from 'axios'
-
-// const fetcher = (key):Partial<SiteMap> => {
-//
-//   console.log('Fetching results from ... local cache???', key)
-//   return (
-//     {
-//     site: {
-//       domain: 'https://www.notion.so/',
-//       name: 'Seeds',
-//       rootNotionPageId: '777bf16ebfc74d4faf96ed28b50de0f6',
-//       rootNotionSpaceId: null,
-//       description: 'SEEDS - for a regenerative world',
-//       timestamp: new Date(),
-//       isDisabled: false
-//     },
-//     pageMap: {
-//       '777bf16e-bfc7-4d4f-af96-ed28b50de0f6': null,
-//       'cbef6961-b022-49a0-9111-505a32a19f95': null,
-//       '1d2ad85a-7e16-47d8-ba75-4c436734fe19': null,
-//       '0057aef5-259f-457c-b731-5ffe04c8153a': null,
-//       '2ec63e24-0592-4b3b-a62c-80fadcb78037': null
-//     },
-//     canonicalPageMap: {
-//       'buying-things': '777bf16e-bfc7-4d4f-af96-ed28b50de0f6',
-//       'last-one': 'cbef6961-b022-49a0-9111-505a32a19f95',
-//       cars: '1d2ad85a-7e16-47d8-ba75-4c436734fe19',
-//       'empty-subpage-to-buying-things': '0057aef5-259f-457c-b731-5ffe04c8153a',
-//       'another-subpage': '2ec63e24-0592-4b3b-a62c-80fadcb78037'
-//     },
-//     hierarchy: {
-//       '777bf16e-bfc7-4d4f-af96-ed28b50de0f6': ['0057aef5-259f-457c-b731-5ffe04c8153a', '1d2ad85a-7e16-47d8-ba75-4c436734fe19'],
-//       'cbef6961-b022-49a0-9111-505a32a19f95': [],
-//       '1d2ad85a-7e16-47d8-ba75-4c436734fe19': [],
-//       '0057aef5-259f-457c-b731-5ffe04c8153a': ['2ec63e24-0592-4b3b-a62c-80fadcb78037'],
-//       '2ec63e24-0592-4b3b-a62c-80fadcb78037': []
-//     }
-//   })
-// }
-
-// interface LinkElement {
-//   name: string
-//   target: string
-//   subs?: LinkElement[]
-// }
-
-
 export const Navigation: React.FC<{
   collapsed: boolean
 }> = ({ collapsed}) => {
@@ -76,9 +27,7 @@ export const Navigation: React.FC<{
     const navLvl1 = Nav
 
     function processLinksAt(navLevel) {
-      console.log(`Processing NavLevel with data: ${JSON.stringify(navLevel)}`)
       return navLevel.map((link, i) => {
-      console.log(`Processing ${link.name}`)
         if (!link.subs) {
           return (
             <li key={i}><Link href={link.target}>{link.name}</Link></li>
@@ -110,9 +59,6 @@ export const Navigation: React.FC<{
   }
 
   const links = createNavigation()
-
-  console.log('Links to SubPages: ', links)
-
   return (
     <div className={`${styles.navigation} ${navOpen?styles.open:null} light-mode`}>
       <a onClick={() => setNavOpen(!navOpen)}><FaHamburger/></a>
